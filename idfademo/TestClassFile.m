f571a536a79329fdf1943f6b1cc64d475cf08c7d
xlf999 <1324005894@qq.com>
2018-01-31 14:42:42 +0800

    add test file

---
+++ idfademo/TestClassFile.m
@@ -0,0 +1,13 @@
+//
+//  TestClassFile.m
+//  IDFA_Demo
+//
+//  Created by alan.xia on 2018/1/31.
+//  Copyright © 2018年 alan.xia. All rights reserved.
+//
+
+#import "TestClassFile.h"
+
+@implementation TestClassFile
+
+@end
